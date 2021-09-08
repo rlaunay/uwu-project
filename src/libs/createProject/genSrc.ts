041e53ebff56958c4f16334634d19a4fc8096543
Remi Launay <rlaunay25@gmail.com>
2021-09-08 23:59:28 +0200

    fix ./index.css

--- src/libs/createProject/genSrc.ts
+++ src/libs/createProject/genSrc.ts
@@ -17,7 +17,7 @@ export default async function genSrc(answer: AnswerSetup) {
     'import React from \'react\';',
     'import ReactDOM from \'react-dom\';',
     'import App from \'./App\';',
-    `import 'index.${styleExt}';`,
+    `import './index.${styleExt}';`,
     '',
     'ReactDOM.render(',
     '  <React.StrictMode>',
